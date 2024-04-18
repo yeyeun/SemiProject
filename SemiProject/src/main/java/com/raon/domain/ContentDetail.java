b81adc7175137bf8c03ddeb4dac88bea874c8027
woyelove <woyelove@gmail.com>
2024-04-18 17:54:39 +0900

    여행코스 수정ing

---
+++ SemiProject/src/main/java/com/raon/domain/ContentDetail.java
@@ -0,0 +1,16 @@
+package com.raon.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ContentDetail {
+	private String title;
+	private String contentid;
+	private String firstimage;
+	private String mapx;
+	private String mapy;
+	private String mlevel;
+	private String overview;
+}
